38c22467fa4bbbcbae6c663e2f509bf0f7415b38
Ahmad Ramadhani <ahmadrama251005@gmail.com>
2023-09-24 17:53:54 +0700

    coba

--- koneksi.php
+++ koneksi.php
@@ -3,5 +3,6 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 // mongodb://localhost:27017;
-$client = new MongoDB\Client('mongodb+srv://vercel-admin-user:u16AAQs1j0EbAr8C@cluster0.vtlnjr4.mongodb.net/Cluster0?retryWrites=true&w=majority');
+// $client = new MongoDB\Client('mongodb+srv://vercel-admin-user:u16AAQs1j0EbAr8C@cluster0.vtlnjr4.mongodb.net/Cluster0?retryWrites=true&w=majority');
+$client = new MongoDB\Client('mongodb+srv://vercel-admin-user-651009b70d91c97bac8e5ae4:FCDnImvw1ym2mPgr@cluster0.vtlnjr4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 $spp = $client->spp;
